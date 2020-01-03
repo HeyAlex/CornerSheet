293dfceb51a81a34e7706b8bfb6fe590964412eb
a.fialko <a.fialko@corp.mail.ru>
2020-01-04 00:46:30 +0300

    initial commit

---
+++ app/src/main/java/com/github/heyalex/cornerdrawer/example/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.heyalex.cornerdrawer.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.heyalex.cornerdrawer.example.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
