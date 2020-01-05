5be807eb73a3f70aff4d16b43f6f91e011a89468
a.fialko <a.fialko@corp.mail.ru>
2020-01-06 00:11:36 +0300

    add behavior to intercept touches depends on translation

---
+++ cornerdrawer/src/main/java/com/github/heyalex/CornerDrawerBehavior.kt
@@ -0,0 +1,19 @@
+package com.github.heyalex
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class CornerDrawerBehavior<T : View>(context: Context, attrs: AttributeSet?) :
+    BottomSheetBehavior<T>(context, attrs) {
+
+    override fun onTouchEvent(parent: CoordinatorLayout, child: T, event: MotionEvent): Boolean {
+        if (event.x < child.translationX) {
+            return false
+        }
+        return super.onTouchEvent(parent, child, event)
+    }
+}
