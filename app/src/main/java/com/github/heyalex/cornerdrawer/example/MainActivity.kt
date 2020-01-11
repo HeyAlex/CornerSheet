adf14afab3568a71a1484a6c52331bcaaaa5ea99
a.fialko <a.fialko@corp.mail.ru>
2020-01-11 10:06:23 +0300

    add support sample

--- app/src/main/java/com/github/heyalex/cornerdrawer/example/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/cornerdrawer/example/MainActivity.kt
@@ -2,45 +2,100 @@ package com.github.heyalex.cornerdrawer.example
 
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
-import com.github.heyalex.cornerdrawer.example.R
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.DialogFragment
+import com.github.heyalex.CornerDrawer
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        window.let {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-//            }
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                // for drawing behind status bar
-//                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-//            }
-//
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                //make system bar to be translucent
-//                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//
-//                //make status bar color transparent
-//                window.statusBarColor = Color.TRANSPARENT
-//
-//                var flags = it.decorView.systemUiVisibility
-//                // make dark status bar icons
-//                flags =
-//                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                    // make dark navigation bar icons
-//                    flags =
-//                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-//                }
-//                window.decorView.systemUiVisibility = flags
-//            }
-//        }
+        window.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // for drawing behind status bar
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                //make system bar to be translucent
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
+                //make status bar color transparent
+                window.statusBarColor = Color.TRANSPARENT
+                window.navigationBarColor = Color.TRANSPARENT
+
+                var flags = it.decorView.systemUiVisibility
+                // make dark status bar icons
+                flags =
+                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    // make dark navigation bar icons
+                    flags =
+                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }
+                window.decorView.systemUiVisibility = flags
+            }
+        }
+
+        val cornerDrawer = findViewById<CornerDrawer>(R.id.corner_drawer)
+        val behavior = BottomSheetBehavior.from(cornerDrawer)
+
+        val header = findViewById<LinearLayout>(R.id.header_root)
+
+        header.setOnClickListener {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+
+        var isLight = false
+        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                if(slideOffset < 0.98) {
+                    if(!isLight) {
+                        changeStatusBarIconColor(false)
+                        isLight = true
+                    }
+                } else if(isLight) {
+                    changeStatusBarIconColor(true)
+                    isLight = false
+                }
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                } else {
+                }
+            }
+        })
+    }
+
+    fun changeStatusBarIconColor(isLight: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window?.let {
+                var flags = it.decorView.systemUiVisibility
+                flags = if (isLight) {
+                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                } else {
+                    flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                }
+                it.decorView.systemUiVisibility = flags
+            }
+        }
     }
 }
