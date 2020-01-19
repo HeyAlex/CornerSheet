d98ca6fbc769d4d5f11ef2009dc006ce15de2612
a.fialko <a.fialko@corp.mail.ru>
2020-01-19 22:44:29 +0300

    add sample

--- app/src/main/java/com/github/heyalex/cornerdrawer/example/MainActivity.kt
+++ app/src/main/java/com/github/heyalex/cornerdrawer/example/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatSeekBar
 import androidx.appcompat.widget.Toolbar
 import com.github.heyalex.CornerDrawer
-import com.github.heyalex.CornerDrawerBehavior
+import com.google.android.material.bottomsheet.CornerSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        R.styleable.BottomSheetBehavior_Layout
         window.let {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
@@ -51,8 +52,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val cornerDrawer = findViewById<CornerDrawer>(R.id.corner_drawer)
-        val behavior = BottomSheetBehavior.from(cornerDrawer) as CornerDrawerBehavior
+//        val cornerDrawer = findViewById<CornerDrawer>(R.id.corner_drawer)
+//        val behavior = BottomSheetBehavior.from(cornerDrawer) as CornerSheetBehavior
 
 //        val header = findViewById<LinearLayout>(R.id.header_root)
 //
@@ -60,52 +61,52 @@ class MainActivity : AppCompatActivity() {
 //            behavior.state = BottomSheetBehavior.STATE_EXPANDED
 //        }
 
-        var state = false
-
-        val hello_world = findViewById<TextView>(R.id.hellow_world)
-        hello_world.setOnClickListener {
-            val state1 = if (state) 0 else 1
-            cornerDrawer.setState(state1)
-            state = !state
-        }
-
-        val seekbar = findViewById<AppCompatSeekBar>(R.id.seek_peek_height)
-        seekbar.max = 500
-        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                behavior.setHorizontalPeekHeight(progress, false)
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-            }
-
-        })
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.setNavigationOnClickListener {
-            behavior.state = BottomSheetBehavior.STATE_COLLAPSED
-        }
-
-        var isLight = false
-        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                if (slideOffset < 0.98) {
-                    if (!isLight) {
-                        changeStatusBarIconColor(false)
-                        isLight = true
-                    }
-                } else if (isLight) {
-                    changeStatusBarIconColor(true)
-                    isLight = false
-                }
-            }
-
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-
-            }
-        })
+//        var state = false
+//
+//        val hello_world = findViewById<TextView>(R.id.hellow_world)
+//        hello_world.setOnClickListener {
+//            val state1 = if (state) 0 else 1
+//            cornerDrawer.setState(state1)
+//            state = !state
+//        }
+//
+//        val seekbar = findViewById<AppCompatSeekBar>(R.id.seek_peek_height)
+//        seekbar.max = 500
+//        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+//            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+//                behavior.setHorizontalPeekHeight(progress, false)
+//            }
+//
+//            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+//            }
+//
+//            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+//            }
+//
+//        })
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        toolbar.setNavigationOnClickListener {
+//            behavior.horizontalState = BottomSheetBehavior.STATE_COLLAPSED
+//        }
+//
+//        var isLight = false
+//        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+//            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+//                if (slideOffset < 0.98) {
+//                    if (!isLight) {
+//                        changeStatusBarIconColor(false)
+//                        isLight = true
+//                    }
+//                } else if (isLight) {
+//                    changeStatusBarIconColor(true)
+//                    isLight = false
+//                }
+//            }
+//
+//            override fun onStateChanged(bottomSheet: View, newState: Int) {
+//
+//            }
+//        })
     }
 
     fun changeStatusBarIconColor(isLight: Boolean) {
