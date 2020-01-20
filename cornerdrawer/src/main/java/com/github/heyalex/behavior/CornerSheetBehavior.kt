a7addc8a8dc876cc41a26a1d0c26a3885d0e9530
a.fialko <a.fialko@corp.mail.ru>
2020-01-20 21:45:50 +0300

    add ratio on expanding, also cleanup

--- cornerdrawer/src/main/java/com/github/heyalex/behavior/CornerSheetBehavior.kt
+++ cornerdrawer/src/main/java/com/github/heyalex/behavior/CornerSheetBehavior.kt
@@ -3,17 +3,13 @@ package com.github.heyalex.behavior
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.ColorStateList
-import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.annotation.IntDef
-import androidx.annotation.StyleableRes
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import com.github.heyalex.R
 import com.github.heyalex.lerp
@@ -27,8 +23,8 @@ class CornerSheetBehavior<V : View> :
 
     private var horizontalPeekWidth: Int = -1
     private var expandedWidth: Int = 0
-    private var horizontalState: Int =
-        STATE_EXPANDED
+    private var expandingRatio: Float = 0.2f
+    private var horizontalState: Int = STATE_EXPANDED
 
     private var sheetBackground: MaterialShapeDrawable? = null
 
@@ -36,33 +32,30 @@ class CornerSheetBehavior<V : View> :
 
     @ColorInt
     private var backgroundColor: Int = -1
-//
-//    @ColorInt
-//    private var headerColor: Int = 0
 
     constructor() : super()
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CornerSheetBehavior_Layout)
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.CornerSheetBehavior_Layout)
         horizontalPeekWidth = typedArray.getDimensionPixelSize(
             R.styleable.CornerSheetBehavior_Layout_behavior_horizontal_peekHeight,
             -1
+        )
+
+        expandingRatio = typedArray.getFloat(
+            R.styleable.CornerSheetBehavior_Layout_behavior_horizontalExpandingRatio,
+            0.2f
         )
 
         expandedWidth = typedArray.getDimensionPixelSize(
             R.styleable.CornerSheetBehavior_Layout_behavior_expanded_width,
             0
         )
-//        maxTranslationX = expandedWidth.toFloat()
 
         backgroundColor = typedArray.getColor(
             R.styleable.CornerSheetBehavior_Layout_background_shape_color,
             -1
         )
-//
-//        contentColor = typedArray.getColor(
-//            R.styleable.CornerSheetBehavior_Layout_content_color,
-//            ContextCompat.getColor(context, R.color.corner_drawer_transparent)
-//        )
 
         sheetBackground = MaterialShapeDrawable(
             ShapeAppearanceModel.builder(
@@ -82,17 +75,6 @@ class CornerSheetBehavior<V : View> :
             sheetBackground?.setTint(defaultColor.data)
         }
 
-//        sheetBackground = MaterialShapeDrawable(
-//            ShapeAppearanceModel.builder(
-//                context,
-//                attrs,
-//                R.attr.bottomSheetStyle,
-//                0
-//            ).build()
-//        ).apply {
-//            fillColor = ColorStateList.valueOf(headerColor)
-//        }
-
         typedArray.recycle()
     }
 
@@ -111,23 +93,11 @@ class CornerSheetBehavior<V : View> :
         }
     }
 
-    override fun onAttachedToLayoutParams(layoutParams: CoordinatorLayout.LayoutParams) {
-        super.onAttachedToLayoutParams(layoutParams)
-        viewReference = null
-    }
-
-    override fun onDetachedFromLayoutParams() {
-        super.onDetachedFromLayoutParams()
-        viewReference = null
-    }
-
     fun getView(unit: ((V) -> Unit)) {
         if (viewReference != null && viewReference!!.get() != null) {
             unit.invoke(viewReference!!.get()!!)
         }
     }
-
-    private var isInitialized: Boolean = false
 
     override fun onLayoutChild(parent: CoordinatorLayout, child: V, layoutDirection: Int): Boolean {
         val onLayoutChildResult = super.onLayoutChild(parent, child, layoutDirection)
@@ -136,29 +106,22 @@ class CornerSheetBehavior<V : View> :
 
             ViewCompat.setBackground(child, sheetBackground)
 
-            if(state == BottomSheetBehavior.STATE_EXPANDED) {
+            val invertExpandedValue = child.width - expandedWidth.toFloat()
+            if (state == BottomSheetBehavior.STATE_EXPANDED) {
                 child.translationX = 0f
-//                sheetBackground?.fillColor = ColorStateList.valueOf(contentColor)
                 sheetBackground?.interpolation = 0f
             } else {
-                child.translationX = expandedWidth.toFloat()
+                child.translationX = invertExpandedValue
+                sheetBackground?.interpolation = 1f
             }
+
 
             addBottomSheetCallback(object :
                 BottomSheetCallback() {
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
                     child.translationX =
-                        lerp(expandedWidth.toFloat(), 0f, 0f, 0.15f, slideOffset)
-                    sheetBackground?.interpolation = lerp(1f, 0f, 0f, 0.15f, slideOffset)
-//                    sheetBackground?.fillColor = ColorStateList.valueOf(
-//                        lerpArgb(
-//                            headerColor,
-//                            contentColor,
-//                            0f,
-//                            0.3f,
-//                            slideOffset
-//                        )
-//                    )
+                        lerp(invertExpandedValue, 0f, 0f, expandingRatio, slideOffset)
+                    sheetBackground?.interpolation = lerp(1f, 0f, 0f, expandingRatio, slideOffset)
                 }
 
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -187,13 +150,13 @@ class CornerSheetBehavior<V : View> :
                 val expandedState = (it.width - expandedWidth).toFloat()
                 val collapsed = (it.width - horizontalPeekWidth).toFloat()
 
-                val start = when(state) {
+                val start = when (state) {
                     STATE_COLLAPSED -> collapsed
                     STATE_EXPANDED -> expandedState
                     else -> expandedState
                 }
 
-                val end = when(state) {
+                val end = when (state) {
                     STATE_COLLAPSED -> expandedState
                     STATE_EXPANDED -> collapsed
                     else -> collapsed
@@ -209,6 +172,16 @@ class CornerSheetBehavior<V : View> :
         }
     }
 
+    override fun onAttachedToLayoutParams(layoutParams: CoordinatorLayout.LayoutParams) {
+        super.onAttachedToLayoutParams(layoutParams)
+        viewReference = null
+    }
+
+    override fun onDetachedFromLayoutParams() {
+        super.onDetachedFromLayoutParams()
+        viewReference = null
+    }
+
     @IntDef(
         STATE_EXPANDED,
         STATE_COLLAPSED,
@@ -221,21 +194,5 @@ class CornerSheetBehavior<V : View> :
         const val STATE_EXPANDED = 3
         const val STATE_COLLAPSED = 4
         const val STATE_HIDDEN = 5
-
-        fun getColorStateList(
-            context: Context, attributes: TypedArray, @StyleableRes index: Int
-        ): ColorStateList? {
-            if (attributes.hasValue(index)) {
-                val resourceId = attributes.getResourceId(index, 0)
-                if (resourceId != 0) {
-                    val value = AppCompatResources.getColorStateList(context, resourceId)
-                    if (value != null) {
-                        return value
-                    }
-                }
-            }
-
-            return attributes.getColorStateList(index)
-        }
     }
 }
