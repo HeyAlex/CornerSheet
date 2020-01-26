47cba3727f24d87438319619a3fd8a1d186f0ff1
a.fialko <a.fialko@corp.mail.ru>
2020-01-27 01:18:27 +0300

    update sample

---
+++ app/src/main/java/com/github/heyalex/cornerdrawer/example/ExampleActivity.kt
@@ -0,0 +1,28 @@
+package com.github.heyalex.cornerdrawer.example
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+
+class ExampleActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.example_activity)
+
+        findViewById<Button>(R.id.main).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.photo_picker).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.support_sample).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
