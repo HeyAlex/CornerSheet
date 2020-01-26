079d68338fe83bf47e3df2d21d48c7b6fe678a52
a.fialko <a.fialko@corp.mail.ru>
2020-01-27 01:04:46 +0300

    prepare corner drawer colors

--- cornerdrawer/src/main/java/com/github/heyalex/CornerDrawer.kt
+++ cornerdrawer/src/main/java/com/github/heyalex/CornerDrawer.kt
@@ -1,8 +1,6 @@
 package com.github.heyalex
 
-import android.animation.ValueAnimator
 import android.content.Context
-import android.content.res.ColorStateList
 import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
@@ -11,10 +9,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowInsets
 import android.widget.FrameLayout
+import androidx.annotation.ColorInt
 import androidx.annotation.LayoutRes
 import androidx.core.view.doOnLayout
+import com.github.heyalex.behavior.CornerSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetBehavior.SAVE_ALL
 
 open class CornerDrawer : FrameLayout {
 
@@ -24,6 +23,12 @@ open class CornerDrawer : FrameLayout {
     @LayoutRes
     private var contentViewRes: Int = 0
 
+
+    @ColorInt
+    private var contentColor: Int = 0
+
+    @ColorInt
+    private var headerColor: Int = 0
 
     private val container: FrameLayout
     private lateinit var header: View
@@ -47,13 +52,11 @@ open class CornerDrawer : FrameLayout {
         contentViewRes =
             typedArray.getResourceId(R.styleable.CornerDrawer_content_view, View.NO_ID)
 
-
         typedArray.recycle()
 
         if (headerViewRes != View.NO_ID) {
             header = LayoutInflater.from(context).inflate(headerViewRes, null).apply {
-                layoutParams =
-                    FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+                layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
             }
         }
 
@@ -61,48 +64,32 @@ open class CornerDrawer : FrameLayout {
             content = LayoutInflater.from(context).inflate(contentViewRes, null)
         }
 
-
         container = FrameLayout(context).apply {
             layoutParams =
-                FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+                LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         }
         super.addView(header)
         super.addView(container)
         addView(content)
 
         doOnLayout {
-//            val bottomSheetBehavior = CornerDrawe.from(this)
-//            bottomSheetBehavior.saveFlags = SAVE_ALL
-//            bottomSheetBehavior.peekHeight = header.height + bottomInset
-//            maxTranslationX = (width - header.width).toFloat()
-//            onStartState()
-//
-//            bottomSheetBehavior.addBottomSheetCallback(object :
-//                BottomSheetBehavior.BottomSheetCallback() {
-//                override fun onSlide(bottomSheet: View, slideOffset: Float) {
-//                    translationX =
-//                        lerp(maxTranslationX, 0f, 0f, 0.15f, slideOffset)
-//                    container.translationY = lerp(0f, topInset.toFloat(), 0.75f, 1f, slideOffset)
-//                    sheetBackground.interpolation = lerp(1f, 0f, 0f, 0.15f, slideOffset)
-//                    sheetBackground.fillColor = ColorStateList.valueOf(
-//                        lerpArgb(
-//                            headerColor,
-//                            contentColor,
-//                            0f,
-//                            0.3f,
-//                            slideOffset
-//                        )
-//                    )
-//
-//                    header.alpha = lerp(1f, 0f, 0f, 0.15f, slideOffset)
-//                    header.visibility = if (slideOffset < 0.5) View.VISIBLE else View.GONE
-//                    content.visibility = if (slideOffset > 0.2) View.VISIBLE else View.GONE
-//                    container.alpha = lerp(0f, 1f, 0.2f, 0.8f, slideOffset)
-//                }
-//
-//                override fun onStateChanged(bottomSheet: View, newState: Int) {
-//                }
-//            })
+            val bottomSheetBehavior = BottomSheetBehavior.from(this) as CornerSheetBehavior
+            bottomSheetBehavior.peekHeight = header.height + bottomInset
+            bottomSheetBehavior.setHorizontalPeekHeight(header.width, false)
+            onStartState()
+
+            bottomSheetBehavior.addBottomSheetCallback(object :
+                BottomSheetBehavior.BottomSheetCallback() {
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    header.alpha = lerp(1f, 0f, 0f, 0.15f, slideOffset)
+                    header.visibility = if (slideOffset < 0.5) View.VISIBLE else View.GONE
+                    content.visibility = if (slideOffset > 0.2) View.VISIBLE else View.GONE
+                    container.alpha = lerp(0f, 1f, 0.2f, 0.8f, slideOffset)
+                }
+
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                }
+            })
         }
     }
 
@@ -110,7 +97,7 @@ open class CornerDrawer : FrameLayout {
 
     protected open fun onStartState() {
         if (isExpanded) {
-            translationX = 0f
+//            translationX = 0f
             container.alpha = 1f
             header.alpha = 0f
 //            sheetBackground.fillColor = ColorStateList.valueOf(contentColor)
@@ -139,54 +126,54 @@ open class CornerDrawer : FrameLayout {
         return super.onApplyWindowInsets(insets)
     }
 
-    override fun onSaveInstanceState(): Parcelable {
-        val superState = super.onSaveInstanceState()
-        superState?.let {
-            val customViewSavedState = CornerDrawerSavedState(superState)
-            val bottomSheetBehavior = BottomSheetBehavior.from(this)
-            customViewSavedState.isExpanded = bottomSheetBehavior.state ==
-                BottomSheetBehavior.STATE_EXPANDED
-            return customViewSavedState
-        }
-        return superState
-    }
-
-    override fun onRestoreInstanceState(state: Parcelable) {
-        val customViewSavedState = state as CornerDrawerSavedState
-        if (customViewSavedState.isExpanded) {
-            isExpanded = true
-            BottomSheetBehavior.from(this).state = BottomSheetBehavior.STATE_EXPANDED
-        }
-        super.onRestoreInstanceState(customViewSavedState.superState)
-    }
-
-    protected class CornerDrawerSavedState : BaseSavedState {
-
-        internal var isExpanded: Boolean = false
-
-        constructor(superState: Parcelable) : super(superState)
-
-        private constructor(source: Parcel) : super(source) {
-            isExpanded = source.readInt() == 1
-        }
-
-        override fun writeToParcel(out: Parcel, flags: Int) {
-            super.writeToParcel(out, flags)
-            out.writeInt(if (isExpanded) 1 else 0)
-        }
-
-        companion object CREATOR : Parcelable.Creator<CornerDrawerSavedState> {
-            override fun createFromParcel(source: Parcel): CornerDrawerSavedState {
-                return CornerDrawerSavedState(source)
-            }
-
-            override fun newArray(size: Int): Array<CornerDrawerSavedState?> {
-                return arrayOfNulls(size)
-            }
-        }
-
-        override fun describeContents(): Int {
-            return 0
-        }
-    }
+//    override fun onSaveInstanceState(): Parcelable {
+//        val superState = super.onSaveInstanceState()
+//        superState?.let {
+//            val customViewSavedState = CornerDrawerSavedState(superState)
+//            val bottomSheetBehavior = BottomSheetBehavior.from(this)
+//            customViewSavedState.isExpanded = bottomSheetBehavior.state ==
+//                BottomSheetBehavior.STATE_EXPANDED
+//            return customViewSavedState
+//        }
+//        return superState
+//    }
+//
+//    override fun onRestoreInstanceState(state: Parcelable) {
+//        val customViewSavedState = state as CornerDrawerSavedState
+//        if (customViewSavedState.isExpanded) {
+//            isExpanded = true
+//            BottomSheetBehavior.from(this).state = BottomSheetBehavior.STATE_EXPANDED
+//        }
+//        super.onRestoreInstanceState(customViewSavedState.superState)
+//    }
+//
+//    protected class CornerDrawerSavedState : BaseSavedState {
+//
+//        internal var isExpanded: Boolean = false
+//
+//        constructor(superState: Parcelable) : super(superState)
+//
+//        private constructor(source: Parcel) : super(source) {
+//            isExpanded = source.readInt() == 1
+//        }
+//
+//        override fun writeToParcel(out: Parcel, flags: Int) {
+//            super.writeToParcel(out, flags)
+//            out.writeInt(if (isExpanded) 1 else 0)
+//        }
+//
+//        companion object CREATOR : Parcelable.Creator<CornerDrawerSavedState> {
+//            override fun createFromParcel(source: Parcel): CornerDrawerSavedState {
+//                return CornerDrawerSavedState(source)
+//            }
+//
+//            override fun newArray(size: Int): Array<CornerDrawerSavedState?> {
+//                return arrayOfNulls(size)
+//            }
+//        }
+//
+//        override fun describeContents(): Int {
+//            return 0
+//        }
+//    }
 }
