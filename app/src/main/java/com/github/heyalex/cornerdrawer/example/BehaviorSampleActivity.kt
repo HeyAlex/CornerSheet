a9815f4b21ac6f9a613347524d2f441ea3861a1a
a.fialko <a.fialko@corp.mail.ru>
2020-02-03 21:57:23 +0300

    update behavior sample

--- app/src/main/java/com/github/heyalex/cornerdrawer/example/BehaviorSampleActivity.kt
+++ app/src/main/java/com/github/heyalex/cornerdrawer/example/BehaviorSampleActivity.kt
@@ -1,13 +1,8 @@
 package com.github.heyalex.cornerdrawer.example
 
-import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
-import android.widget.LinearLayout
 import android.widget.SeekBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatSeekBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -19,37 +14,6 @@ class BehaviorSampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.let {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                // for drawing behind status bar
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-            }
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                //make system bar to be translucent
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-
-                //make status bar color transparent
-                window.statusBarColor = Color.TRANSPARENT
-                window.navigationBarColor = Color.TRANSPARENT
-
-                var flags = it.decorView.systemUiVisibility
-                // make dark status bar icons
-                flags =
-                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    // make dark navigation bar icons
-                    flags =
-                        flags xor View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                }
-                window.decorView.systemUiVisibility = flags
-            }
-        }
 
         val cornerDrawer = findViewById<View>(R.id.corner_behavior_container)
         val behavior = BottomSheetBehavior.from(cornerDrawer) as CornerMaterialSheetBehavior
@@ -66,9 +30,8 @@ class BehaviorSampleActivity : AppCompatActivity() {
             behavior.setHorizontalState(CornerMaterialSheetBehavior.STATE_HIDDEN)
         }
 
-
         val seekbar = findViewById<AppCompatSeekBar>(R.id.seek_peek_height)
-        seekbar.max = 1500
+        seekbar.max = 800
         seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 behavior.setHorizontalPeekHeight(progress, true)
@@ -81,45 +44,5 @@ class BehaviorSampleActivity : AppCompatActivity() {
             }
 
         })
-
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            toolbar.setNavigationOnClickListener {
-                behavior.state = BottomSheetBehavior.STATE_COLLAPSED
-            }
-        }
-
-        var isLight = false
-        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                if (slideOffset < 0.98) {
-                    if (!isLight) {
-                        changeStatusBarIconColor(false)
-                        isLight = true
-                    }
-                } else if (isLight) {
-                    changeStatusBarIconColor(true)
-                    isLight = false
-                }
-            }
-
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-
-            }
-        })
-    }
-
-    fun changeStatusBarIconColor(isLight: Boolean) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            window?.let {
-                var flags = it.decorView.systemUiVisibility
-                flags = if (isLight) {
-                    flags xor View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                } else {
-                    flags or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                }
-                it.decorView.systemUiVisibility = flags
-            }
-        }
     }
 }
